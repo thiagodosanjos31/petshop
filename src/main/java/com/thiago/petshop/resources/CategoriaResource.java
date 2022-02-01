ad1bf5f839fd2debb0140a6452ba60dd6245a1ec
Thiago dos Anjos <thiagoadosanjos@outlook.com.br>
2022-01-31 21:32:30 -0300

    Adicionado a classe de modelo 'Categoria'

--- src/main/java/com/thiago/petshop/resources/CategoriaResource.java
+++ src/main/java/com/thiago/petshop/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.thiago.petshop.resources;
 
+import com.thiago.petshop.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String mostrar(){
-        return "Teste REST Categorias";
+    public List<Categoria> mostrar(){
+        Categoria cat1 = new Categoria(1, "Alimento");
+        Categoria cat2 = new Categoria(2, "Cosmetico");
+        Categoria cat3 = new Categoria(3, "Remedio");
+
+        List<Categoria> categorias = new ArrayList<>();
+        categorias.add(cat1);
+        categorias.add(cat2);
+        categorias.add(cat3);
+
+        return categorias;
     }
 }
