73f3cf79c4b6399613976e0dc641d69114b146c0
Thiago dos Anjos <thiagoadosanjos@outlook.com.br>
2022-01-31 21:15:21 -0300

    Inclui fachada de categorias

---
+++ src/main/java/com/thiago/petshop/resources/CategoriaResource.java
@@ -0,0 +1,15 @@
+package com.thiago.petshop.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String mostrar(){
+        return "Teste REST Categorias";
+    }
+}
